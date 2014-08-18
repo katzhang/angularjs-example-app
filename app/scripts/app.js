2a2584951898ce1a728bbbb60c69e10113f69265
katzhang <arcadychang@gmail.com>
2014-08-18 19:53:12 -0400

    scaffolded with yeoman angular generator

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name exampleApp
+ * @description
+ * # exampleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('exampleApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
