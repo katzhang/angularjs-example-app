88c0bcb1729b240f0cbb28080b24a92408b953cb
katzhang <arcadychang@gmail.com>
2014-08-18 21:15:18 -0400

    exploring Module methods

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,26 +8,29 @@
  *
  * Main module of the application.
  */
-angular
-  .module('exampleApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+var myApp = angular.module('exampleApp', []);
+
+myApp.controller('dayCtrl', function($scope) {
+  var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  $scope.day = dayNames[new Date().getDay()];
+});
+
+myApp.controller("tomorrowCtrl", function ($scope) {
+  var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  $scope.day = dayNames[(new Date().getDay() + 1) % 7];
+});
+
+myApp.directive('highlight', function() {
+  return function(scope, element, attrs) {
+    if (scope.day == attrs['highlight']) {
+      element.css('color', 'red');
+    }
+  }
+});
+
+myApp.filter('dayName', function() {
+  var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return function (input) {
+    return angular.isNumber(input) ? dayNames[input] : input;
+  }
+});
