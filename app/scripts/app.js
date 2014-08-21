d2eb1e5511ec50e7e2915f26ea8cfae75c5bc4b4
katzhang <arcadychang@gmail.com>
2014-08-20 22:59:58 -0400

    playing around with directives

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,37 +8,13 @@
  *
  * Main module of the application.
  */
-var myApp = angular.module('exampleApp', []);
-
-myApp.controller('dayCtrl', function($scope, days) {
-  $scope.day = days.today;
-});
-
-myApp.controller("tomorrowCtrl", function ($scope, days) {
-  $scope.day = days.tomorrow;
-});
-
-myApp.directive('highlight', function($filter) {
-  var dayFilter = $filter('dayName');
-
-  return function(scope, element, attrs) {
-    if (dayFilter(scope.day) == attrs['highlight']) {
-      element.css('color', 'red');
-    }
-  }
-});
-
-myApp.filter('dayName', function() {
-  var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  return function (input) {
-    return angular.isNumber(input) ? dayNames[input] : input;
-  }
-});
-
-var now = new Date();
-myApp.value("nowValue", now);
-
-myApp.service('days', function(nowValue) {
-  this.today = nowValue.getDay();
-  this.tomorrow = this.today + 1;
-})
+angular.module('exampleApp', [])
+  .controller('defaultCtrl', function ($scope) {
+    $scope.todos = [
+      {action: 'Get groceries', complete: false},
+      {action: 'Call plumber', complete: false},
+      {action: 'Buy running shoes', complete: false},
+      {action: 'Buy flowers', complete: false},
+      {action: 'Call family', complete: false}
+    ]
+  })
