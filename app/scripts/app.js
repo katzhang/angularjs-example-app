3a3e64f41fbf700d613b37a8aa4bbd3371843206
katzhang <arcadychang@gmail.com>
2014-08-26 13:54:32 -0400

    playing around with controllers inheritance

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,28 +8,3 @@
  *
  * Main module of the application.
  */
-angular.module('exampleApp', [])
-  .service("ZipCodes", function($rootscope) {
-    return {
-      setZipCode: function(type, zip) {
-        this[type] = zip;
-        $rootscope.$broadcast("zipCodeUpdated", {
-          type: type, zipCode: zip
-        })
-      }
-    }
-  })
-  .controller("simpleCtrl", function ($scope, ZipCodes) {
-    $scope.$on("zipCodeUpdated", function (event, args) {
-      $scope[args.type] = args.zipCode;
-    });
-
-    $scope.setAddress = function (type, zip) {
-      ZipCodes.setZipCode(type, zip);
-      console.log("Type: " + type, zip);
-    }
-
-    $scope.copyAdress = function () {
-      $scope.zip = $scope.billingZip;
-    }
-  });
