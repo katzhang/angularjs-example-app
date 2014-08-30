0d3fe1ce08023e173e327b0b9e3c7e4bd627460d
katzhang <arcadychang@gmail.com>
2014-08-29 23:14:12 -0400

    defining complex directives

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,21 +8,26 @@
  * Controller of the exampleApp
  */
 angular.module('exampleApp', [])
+	.directive('unorderedList', function () {
+		return function (scope, element, attrs) {
+			var data = scope[attrs['unorderedList']];
+			var propertyExpression = attrs['listProperty'];
+			if (angular.isArray(data)) {
+				var listElem = angular.element('<ul>');
+				element.append(listElem);
+				for (var i = 0; i < data.length; i++) {
+					var itemElement = angular.element('<li>')
+						.text(scope.$eval(propertyExpression, data[i]));
+					listElem.append(itemElement);
+				}
+			}
+		}
+	})
 	.controller('defaultCtrl', function ($scope) {
 		$scope.products = [
 			{ name: "Apples", category: "Fruit", price: 1.20, expiry: 10 },
 			{ name: "Bananas", category: "Fruit", price: 2.42, expiry: 7 },
-			{ name: "Pears", category: "Fruit", price: 2.02, expiry: 6 },
-			{ name: "Tuna", category: "Fish", price: 20.45, expiry: 3 },
-			{ name: "Salmon", category: "Fish", price: 17.93, expiry: 2 },
-			{ name: "Trout", category: "Fish", price: 12.93, expiry: 4 },
-			{ name: "Beer", category: "Drinks", price: 2.99, expiry: 365 },
-			{ name: "Wine", category: "Drinks", price: 8.99, expiry: 365 },
-			{ name: "Whiskey", category: "Drinks", price: 45.99, expiry: 365 }
+			{ name: "Pears", category: "Fruit", price: 2.02, expiry: 6 }
 		];
 
-		$scope.getExpiryDate = function (days) {
-			var now = new Date();
-			return now.setDate(now.getDate() + days);
-		}
 	})
