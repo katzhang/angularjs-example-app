33830a7767294d861b75c04aaf2022d31f783a3b
katzhang <arcadychang@gmail.com>
2014-09-02 20:56:47 -0400

    using transclusion for directives

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,15 +8,6 @@
  * Controller of the exampleApp
  */
 angular.module('exampleApp', [])
-	.directive('unorderedList', function () {
-		return {
-			link: function (scope, element, attrs) {
-				scope.data = scope[attrs['unorderedList']];
-			},
-			restrict: 'A',
-			template: "<ul><li ng-repeat='item in data'>" + "{{item.price | currency}}</li></ul>"
-		}
-	})
 	.controller('defaultCtrl', function ($scope) {
 		$scope.products = [
 			{ name: "Apples", category: "Fruit", price: 1.20, expiry: 10 },
@@ -24,4 +15,35 @@ angular.module('exampleApp', [])
 			{ name: "Pears", category: "Fruit", price: 2.02, expiry: 6 }
 		];
 
+		$scope.changeData = function () {
+			$scope.products.push({ name: "Cherries", price: 4.02 });
+			for (var i = 0; i < $scope.products.length; i++) {
+				$scope.products[i].price++;
+			}
+		}
+
+	})
+	.directive('simpleRepeater', function() {
+		return {
+			scope: {
+				data: "=source",
+				propName: "@itemName"
+			},
+			transclude: 'element',
+			compile: function (element, attrs, transcludeFn) {
+				return function ($scope, $element, $attr) {
+					$scope.$watch("data.length", function () {
+						var parent = $element.parent();
+						parent.children().remove();
+						for (var i = 0; i < $scope.data.length; i++) {
+							var childScope = $scope.$new();
+							childScope[$scope.propName] = $scope.data[i];
+							transcludeFn(childScope, function (clone) {
+								parent.append(clone);
+							})
+						}
+					})
+				}
+			}
+		}
 	})
