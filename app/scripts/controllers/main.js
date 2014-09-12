bc35b4876233d4e2ae45bda4c45eb8a2838770ef
katzhang <arcadychang@gmail.com>
2014-09-11 22:08:06 -0400

    playing around with ajax services

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,23 +9,13 @@
  */
 
 angular.module('exampleApp', [])
-	.controller('defaultCtrl', function ($scope) {
+	.controller('defaultCtrl', function ($scope, $http) {
 
-		$scope.price = "100.23";
-	})
-	.directive('evalExpression', function ($parse) {
-		return function(scope, element, attrs) {
-			scope.$watch(attrs['evalExpression'], function (newValue) {
-				try {
-					var expressionFn = $parse(scope.expr);
-					var result = expressionFn(scope);
-					if (result == undefined) {
-						result = "no result";
-					}
-				} catch(err) {
-					result = "cannot evaluate expression";
-				}
-				element.text(result);
+		$scope.loadData = function () {
+			$http.get('scripts/productData.json').then(function (data) {
+				console.log(data);
+				$scope.products = data;
 			})
 		}
 	})
+
